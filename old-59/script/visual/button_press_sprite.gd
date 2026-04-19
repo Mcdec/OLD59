c05d47ae995a457ced4689b9b935171a011aab7a
Parafrin <gbysslab@gmail.com>
2026-04-20 02:27:13 +0600

    more sounds (sound logic not yet finished)

--- old-59/script/visual/button_press_sprite.gd
+++ old-59/script/visual/button_press_sprite.gd
@@ -3,12 +3,15 @@ extends Sprite2D
 
 @onready var sprite_pushed: Sprite2D = $"../sprite_pushed"
 @onready var button: Button = $".."
+@onready var visual: Visual = $"../.."
 
-#@export var
+var sp_button : AudioStreamPlayer2D
+var sounds_button := Visual.get_sounds("button")
 
 var timer := Timer.new()
 
 func _ready() -> void:
+	sp_button = Visual.add_sound_player(self,"button")
 	button.pressed.connect(button_down)
 	timer.one_shot = true
 	timer.timeout.connect(button_up)
@@ -18,6 +21,7 @@ func button_down() -> void:
 	visible = false
 	sprite_pushed.visible = true
 	timer.start(0.35)
+	Visual.play_spatial_sound(sp_button,sounds_button,0)
 
 func button_up() -> void:
 	visible = true

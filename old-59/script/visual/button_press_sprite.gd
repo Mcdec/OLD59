c07e9343e8f38c359377cd76df4031c7d96543fc
Dmitry <dudikov05@gmail.com>
2026-04-19 23:17:43 +0600

    Revert "дай бог ничего не поломать"
    This reverts commit b5ab20e79180425d77ef023887c6f1cce1686d0f, reversing
    changes made to 81a1225faef2ac612a3d7ce23287d286633dffff.

--- old-59/script/visual/button_press_sprite.gd
+++ old-59/script/visual/button_press_sprite.gd
@@ -6,18 +6,13 @@ extends Sprite2D
 
 #@export var
 
-var timer := Timer.new()
-
 func _ready() -> void:
-	button.pressed.connect(button_down)
-	timer.one_shot = true
-	timer.timeout.connect(button_up)
-	button.add_child.call_deferred(timer)
+	button.button_down.connect(button_down)
+	button.button_up.connect(button_up)
 
 func button_down() -> void:
 	visible = false
 	sprite_pushed.visible = true
-	timer.start(0.35)
 
 func button_up() -> void:
 	visible = true
